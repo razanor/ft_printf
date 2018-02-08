046425c325530f41839604608608011e65309c95
Nazarii Repak <nazara.re22@gmail.com>
2018-02-08 15:17:31 +0200

    Norme 80% done

--- octal.c
+++ octal.c
@@ -6,37 +6,44 @@
 /*   By: nrepak <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/29 14:55:33 by nrepak            #+#    #+#             */
-/*   Updated: 2018/01/29 14:55:37 by nrepak           ###   ########.fr       */
+/*   Updated: 2018/02/08 15:14:30 by nrepak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static char		*ft_precision(uintmax_t i, char *str, char **num, t_flags *f)
+{
+	int		a;
+	int		len;
+
+	len = 0;
+	a = f->precision - ft_base_len(i, 8);
+	while (a--)
+		str[len++] = '0';
+	a = 0;
+	while ((*num)[a])
+		str[len++] = (*num)[a++];
+	ft_strdel(&(*num));
+	return (str);
+}
+
 static char		*ft_precision_slash(uintmax_t i, t_flags *f)
 {
-	char *str;
-	char *num;
-	int  len;
-	int  flag;
-	int  a;
+	char	*str;
+	char	*num;
+	int		len;
+	int		flag;
+	int		a;
 
 	len = 0;
 	num = ft_itoa_unsigned(i, 8);
-	(f->slash && i && f->precision < ft_base_len(i, 8)) ? (flag = 1) : (flag = 0);
+	flag = (f->slash && i && f->precision < ft_base_len(i, 8)) ? 1 : 0;
 	f->precision > ft_base_len(i, 8) ? (str = ft_strnew(f->precision)) :
 		(str = ft_strnew(ft_base_len(i, 8) + flag));
 	if (f->precision && f->precision > ft_base_len(i, 8))
-	{
-		a = f->precision - ft_base_len(i, 8);
-		while (a--)
-			str[len++] = '0';
-		a = 0;
-		while (num[a])
-			str[len++] = num[a++];
-		ft_strdel(&num);
-		return (str);
-	}
-	(flag == 1) ? (str[len++] = '0') : (str[len] = '\0');
+		return (ft_precision(i, str, &num, f));
+	(flag == 1) ? (str[len++] = '0') : 1;
 	a = 0;
 	while (num[a])
 		str[len++] = num[a++];
@@ -44,11 +51,11 @@ static char		*ft_precision_slash(uintmax_t i, t_flags *f)
 	return (str);
 }
 
-static int ft_width(intmax_t i, int len, char **str, t_flags *f)
+static int		ft_width(intmax_t i, int len, char **str, t_flags *f)
 {
 	if (f->minus)
 	{
-		write (1, *str, len);
+		write(1, *str, len);
 		ft_put_specific_char(' ', f->width - len);
 	}
 	else if (f->zero && !f->precision && !f->zero_precision)
@@ -65,16 +72,16 @@ static int ft_width(intmax_t i, int len, char **str, t_flags *f)
 	else
 	{
 		ft_put_specific_char(' ', f->width - len);
-		write (1, *str, len);
+		write(1, *str, len);
 	}
 	ft_strdel(&(*str));
 	return (f->width);
 }
 
-static	int ft_collect_for_Oo(uintmax_t i, t_flags *f)
+static int		ft_collect_for_octal(uintmax_t i, t_flags *f)
 {
-	char *str;
-	int	 len;
+	char	*str;
+	int		len;
 
 	str = ft_precision_slash(i, f);
 	len = ft_strlen(str);
@@ -85,22 +92,22 @@ static	int ft_collect_for_Oo(uintmax_t i, t_flags *f)
 		ft_strdel(&str);
 		return (0);
 	}
-	write (1, str, len);
+	write(1, str, len);
 	ft_strdel(&str);
 	return (len);
 }
-	
-int	ft_octal(va_list lst, char c, t_flags *f)
+
+int				ft_octal(va_list lst, char c, t_flags *f)
 {
 	uintmax_t i;
 
 	i = va_arg(lst, uintmax_t);
 	if (c == 'o')
 	{
-		CAST ? (i = ft_cast_to_unsigned(i, f)) : (i = (unsigned int)i);	
-		return (ft_collect_for_Oo(i, f));
+		i = (CAST) ? (ft_cast_to_unsigned(i, f)) : (unsigned int)i;
+		return (ft_collect_for_octal(i, f));
 	}
 	if (c == 'O')
-		return (ft_collect_for_Oo(i, f));
+		return (ft_collect_for_octal(i, f));
 	return (0);
 }
